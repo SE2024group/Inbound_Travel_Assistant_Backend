2603583d75d237df9313769b78e3e1ea3b5a4ea2
Songard_ <1543608534@qq.com>
2024-12-14 23:36:32 +0800

    Feature: Now can storage the avatar

--- web/users/models.py
+++ web/users/models.py
@@ -3,10 +3,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
+import os
+import uuid
+
+
+def user_avatar_upload_to(instance, filename):
+    # 获取文件扩展名
+    ext = os.path.splitext(filename)[1]
+    # 使用uuid生成唯一文件名
+    new_filename = f"{uuid.uuid4().hex}{ext}"
+    return os.path.join('avatars', str(instance.id), new_filename)
 
 class CustomUser(AbstractUser):
     nickname = models.CharField(max_length=150, blank=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    avatar = models.ImageField(upload_to=user_avatar_upload_to, blank=True, null=True)
     personality_description = models.TextField(blank=True, null=True)
     signup_date = models.DateField(null=True, blank=True, help_text="用户注册日期（年月日）")
     
